ed41d45ae8477932d0288023f87f4ec6ece5778d
Noe CM <128952542+NoeAV1314@users.noreply.github.com>
2024-02-01 06:02:29 +0000

    No comprendo el fetch

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,25 +1,81 @@
-import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, { useEffect,useState } from "react";
 
 //create your first component
 const Home = () => {
+	const [inputValue, setInputValue] = useState("")
+	const [todos, setTodos] = useState([])
+	
+
+
+	// const actualizarListaEnServidor = (nuevasTareas) => {
+	// 	fetch('https://psychic-capybara-qwv7qrgw6xx2jrg-3000.app.github.dev/', {
+	// 	  method: "PUT",
+	// 	  body: JSON.stringify(nuevasTareas),
+	// 	  headers: {
+	// 		"Content-Type": "application/json"
+	// 	  }
+	// 	})
+	// 	.then(resp => resp.json())
+	// 	.then(data => console.log('Lista actualizada en el servidor:', data))
+	// 	.catch(error => console.error('Error al actualizar la lista en el servidor:', error));
+	//   };
+	  
+	//   const agregarNuevaTarea = () => {
+	// 	// Añadir la nueva tarea al estado 'todos'
+	// 	setTodos(todos.concat(inputValue));
+	  
+	// 	// Realizar una solicitud PUT para actualizar la lista en el servidor
+	// 	actualizarListaEnServidor(todos.concat(inputValue));
+	  
+	// 	// Limpiar el campo de entrada después de agregar la tarea
+	// 	setInputValue("");
+	//   };
+	  
+	//   const eliminarTarea = (index) => {
+	// 	// Filtrar las tareas para eliminar la tarea en el índice proporcionado
+	// 	const nuevasTareas = todos.filter((_, currentIndex) => index !== currentIndex);
+	  
+	// 	// Actualizar el estado 'todos' con las nuevas tareas
+	// 	setTodos(nuevasTareas);
+	  
+	// 	// Realizar una solicitud PUT para actualizar la lista en el servidor
+	// 	actualizarListaEnServidor(nuevasTareas);
+	//   };
+
+
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+		<div className="container">
+			<h1>Todo list using React + Fetch </h1>
+			<ul>
+				<li>
+					<input type="text" 
+					// event = e. Así que el "e.target.value" es la version corta de "event.target.value"
+					// el "value" de los inputs/events hace referencia a literalmente lo que estamos escribiendo en el input
+					onChange={(e) => setInputValue(e.target.value)} 
+					value={inputValue}
+					onKeyPress={(e) => 
+						e.key === "Enter" 
+						? agregarNuevaTarea()
+						: null
+					}
+					placeholder="What do you need to do?"></input>
+				</li>
+				
+				{todos.map((item, index) => (
+					<li> 
+						{item} {" "} <i className="fa-solid fa-eraser" style={{color: "#FFB650"}} 
+						onClick={() => 
+							eliminarTarea (index)	
+						}></i>
+					</li>
+				))}
+
+				
+			
+			</ul>
+			<div className="leftTask"> {todos.length} left.</div>
+		</div >
 	);
 };
 
